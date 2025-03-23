7af812dec06d5e43342144a9fe5161dfc861f6cc
RaflyAdiyasa <rafly271008@gmail.com>
2025-03-23 09:01:18 +0700

    add Dockerfile

--- Backend/config/Database.js
+++ Backend/config/Database.js
@@ -1,8 +1,9 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('catatanku', 'root','',{
-    host: 'localhost',
-    dialect : 'mysql'
-})
+const db = new Sequelize("catatanku", "rafly", "pass123", {
+    host: "34.172.143.47",
+    dialect: "mysql",
+  });
+
 
 export default db;
